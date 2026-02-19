5e8e90951215c8f2260c79b473939a7718b13614
Eduard Remets <Eduard.Remets@tpt.lan>
2026-02-19 12:48:13 +0200

    do some improvements

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 const ipinput = document.querySelector(".ipinput");
 const getgeobtn = document.querySelector(".getgeobtn");
-const infoparagraph = document.querySelector(".infoparagraph");
-const countryparagraph = document.querySelector(".countryparagraph");
-const organizationparagraph = document.querySelector(".organizationparagraph");
-const latitudeparagraph = document.querySelector(".latitudeparagraph");
-const longitudeparagraph = document.querySelector(".longitudeparagraph");
-const timezoneparagraph = document.querySelector(".timezoneparagraph");
+const box = document.querySelector(".box");
 
 async function getip2location(ip) {
     const response = await fetch('https://apip.cc/api-json/' + ip);
     const data = await response.json();
     const datastring = JSON.stringify(data);
-    // infoparagraph.textContent = datastring;
-    countryparagraph.textContent = "Country: " + data.CountryName;
-    organizationparagraph.textContent = "Organization: " + data.org;
-    latitudeparagraph.textContent = "Latitude: " + data.Latitude;
-    longitudeparagraph.textContent = "Longitude: " + data.Longitude;
-    timezoneparagraph.textContent = "Timezone: " + data.TimeZone;
+    // dataparagraph.textContent = datastring;
+    let paragraphsdiv = document.querySelector(".paragraphsdiv");
+    let dataparagraph = document.querySelector(".dataparagraph");
+    let infoparagraph = document.querySelector(".infoparagraph");
+    if (!paragraphsdiv) {
+        paragraphsdiv = document.createElement('div');
+        paragraphsdiv.className = "paragraphsdiv";
+        dataparagraph = document.createElement('p');
+        dataparagraph.className = "dataparagraph";
+        infoparagraph = document.createElement('p');
+        infoparagraph.className = "infoparagraph";
+        paragraphsdiv.appendChild(dataparagraph);
+        paragraphsdiv.appendChild(infoparagraph);
+        box.appendChild(paragraphsdiv);
+    }
+    if (data.status !== "success") {
+        dataparagraph.textContent = "Error";
+        return;
+    }
+    infoparagraph.textContent = "Country: " + data.CountryName + "\n\n" 
+        + "Organization: " + data.org + "\n\n" 
+        + "Latitude: " + data.Latitude + "\n\n"
+        + "Longitude: " + data.Longitude + "\n\n";
+        + "Timezone: " + data.TimeZone;
 }
 
 getgeobtn.addEventListener('click', function() {
     getip2location(ipinput.value);
 });
-
